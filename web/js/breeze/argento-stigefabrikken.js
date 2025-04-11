3853db60561e64bcd81f0525718c44083a975d6a
rommmka <rommmmka@gmail.com>
2025-04-11 17:25:39 +0300

    Homepage: stying and content changes

--- web/js/breeze/argento-stigefabrikken.js
+++ web/js/breeze/argento-stigefabrikken.js
@@ -1,10 +1,8 @@
 (() => {
     'use strict';
-
     $(document).on('breeze:load', () => {
         $.async('.breeze-carousel .easycatalogimg', (el) => {
             $.onReveal(el, () => $(el).pagebuilderCarousel());
         });
     });
-
 })();
